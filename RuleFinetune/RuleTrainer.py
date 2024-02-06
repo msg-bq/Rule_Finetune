5dbb8b9d58093b29513689886138ad742d74ea89
msg-bq <82528553+msg-bq@users.noreply.github.com>
2024-02-06 19:38:07 +0800

    a

--- RuleFinetune/RuleTrainer.py
+++ RuleFinetune/RuleTrainer.py
@@ -128,7 +128,7 @@ class Trainer:
     def train(self):
         bandits, question2cluster = demo_cluster(self.args, self.train_dataset)
         # 这个地方还不好替换成别的方案，封装的不是特别特别充分
-        save_path = f"./experiment/train_loss.txt"
+        save_path = f"{self.args.save_dir}/train_loss.txt"
         with open(save_path, 'w', encoding="utf8") as f:
             pass
 
@@ -150,14 +150,14 @@ class Trainer:
                 # 这条限制，是可以保留或者说可控的。这种过拟合也比参数微调方便控制
 
             # 保存self.rule_base._rule_name_2_rule_instance
-            self.rule_base.save(f"./experiment/rule_base_epoch{ep}")
+            self.rule_base.save(f"{self.args.save_dir}/rule_base_epoch{ep}")
             # 保存bandits
             for k, v in bandits.items():
-                v.save(f"./experiment/bandits_epoch{ep}_{k}")
+                v.save(f"{self.args.save_dir}/bandits_epoch{ep}_{k}")
 
             self.rule_base.broadcast_rule_info() # 每个epoch统一平均，避免并行带来的不同步
 
-        self.rule_base.save(f"./experiment/rule_base_final")
+        self.rule_base.save(f"{self.args.save_dir}/rule_base_final")
 
     def eval(self):
         """
@@ -172,20 +172,20 @@ class Trainer:
         # 2. 读入rule_base
         if len(self.rule_base) == 0:  # 这个我觉得是，只有纯测试的时候才需要读入。平时的话规则库本来就在训练过程中有了
             # 如果还想考虑一个特殊情况的话，就是checkpoint。但是这个我觉得也没必要，因为checkpoint的load阶段就应该读入了
-            with open(f"./experiment/rule_base_final", encoding="utf8") as f:
+            with open(f"{self.args.save_dir}/rule_base_final", encoding="utf8") as f:
                 rules = [l for l in f.readlines() if l.strip()]
                 self.rule_base.read_rules(rules)
 
         # 3. 读入train_dataset的rationale
         # 目前还没保存训练集，所以直接读取demos和added_rules
-        with open(f"./experiment/demos_epoch{0}", encoding="utf8") as f:
+        with open(f"{self.args.save_dir}/demos_epoch{0}", encoding="utf8") as f:
             self.eval_demos = "".join([l for l in f.readlines()])
 
-        save_path = f"./experiment/demos_eval"
+        save_path = f"{self.args.save_dir}/demos_eval"
         with open(save_path, 'w', encoding="utf8") as f:
             f.write(str(self.eval_demos))
 
-        with open(f"./experiment/added_rule_epoch{0}", encoding="utf8") as f:
+        with open(f"{self.args.save_dir}/added_rule_epoch{0}", encoding="utf8") as f:
             self.eval_added_rules = "".join([l for l in f.readlines()])
 
     def eval_step(self, example: Example):
@@ -227,9 +227,9 @@ class Trainer:
         pass
 
     def force_write(self, ep):
-        save_path = f"./experiment/rule_base_epoch{ep}"
-        added_rules_save_path = f"./experiment/added_rule_epoch{ep}"
-        demos_save_path = f"./experiment/demos_epoch{ep}"
+        save_path = f"{self.args.save_dir}/rule_base_epoch{ep}"
+        added_rules_save_path = f"{self.args.save_dir}/added_rule_epoch{ep}"
+        demos_save_path = f"{self.args.save_dir}/demos_epoch{ep}"
         for p in [save_path, added_rules_save_path, demos_save_path]:
             if os.path.exists(p):
                 os.remove(p)
